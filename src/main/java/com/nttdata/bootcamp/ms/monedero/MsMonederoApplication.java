20a82c43e3139207c0b8ff662f8c0d988e88b124
evillarv <esthervillarv@gmail.com>
2023-02-01 16:59:14 -0500

    first upload

---
+++ src/main/java/com/nttdata/bootcamp/ms/monedero/MsMonederoApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.ms.monedero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMonederoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsMonederoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/bootcamp/ms/monedero/MsMonederoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.ms.monedero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsMonederoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
